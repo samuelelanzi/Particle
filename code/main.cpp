40e4c74317c03125014b42a553ba5e93fd4ff905
samuelelanzi <samuelelanzi00@icloud.com>
2020-11-10 13:08:25 +0100

    upload

--- code/main.cpp
+++ code/main.cpp
@@ -1,8 +1,8 @@
 // header file
-#include "invMass.hpp"
 #include "Particle.hpp"
 #include "ParticleType.hpp"
 #include "ResonanceType.hpp"
+#include "invMass.hpp"
 #include "rndmCharge.hpp"
 
 // ROOT's header file
@@ -18,22 +18,22 @@
 int main() {
   using namespace std;
 
-  ParticleType* pion = new ParticleType{"pion", 0.13957, 0}; // 0 is default
-  ParticleType* kaon = new ParticleType{"kaon", 0.49367, 0};
-  ParticleType* proton = new ParticleType{"proton", 0.93827, 0};
-  ParticleType* K_s = new ParticleType{"K*", 0.89166, 0};
-  ResonanceType* K_resonance = new ResonanceType{*K_s, 0.050};
+  ParticleType *pion = new ParticleType{"pion", 0.13957, 0}; // 0 is default
+  ParticleType *kaon = new ParticleType{"kaon", 0.49367, 0};
+  ParticleType *proton = new ParticleType{"proton", 0.93827, 0};
+  ParticleType *K_s = new ParticleType{"K*", 0.89166, 0};
+  ResonanceType *K_resonance = new ResonanceType{*K_s, 0.050};
 
   vector<Particle> particle_v{};
 
   gStyle->SetOptStat(112210);
   gStyle->SetOptFit(111);
 
-  TH1F* hPhi = new TH1F("hPhi", "Phi Distribution", 100, 0., 2 * M_PI);
-  TH1F* hTheta = new TH1F("hTheta", "Theta Distribution", 100, 0., M_PI);
-  TH1F* hP = new TH1F("hP", "Momentum Distribution", 1000, 0, 7);
-  TH1F* hPtr = new TH1F("hPtr", "Trasversal Momentum Distribution", 1000, 0, 5);
-  TH1F* hE = new TH1F("hE", "Energy Distribution", 1000, 0, 6);
+  TH1F *hPhi = new TH1F("hPhi", "Phi Distribution", 100, 0., 2 * M_PI);
+  TH1F *hTheta = new TH1F("hTheta", "Theta Distribution", 100, 0., M_PI);
+  TH1F *hP = new TH1F("hP", "Momentum Distribution", 1000, 0, 7);
+  TH1F *hPtr = new TH1F("hPtr", "Trasversal Momentum Distribution", 1000, 0, 5);
+  TH1F *hE = new TH1F("hE", "Energy Distribution", 1000, 0, 6);
   TH1F *hPT = new TH1F("hPT", "Particle Types Distribution", 4, 0, 5);
 
   random_device rd;
@@ -64,8 +64,10 @@ int main() {
         hPT->Fill(1);
         pion->setCharge(charge);
         Particle pi{pion, "pion", pi_linearMomentum};
-        pi.setP(p_ * sin(theta) * cos(phi), p_ * sin(theta) * sin(phi), p_ * cos(theta));
-        hPtr->Fill(sqrt(pow(p_ * sin(theta) * cos(phi), 2) + pow(p_ * sin(theta) * sin(phi), 2)));
+        pi.setP(p_ * sin(theta) * cos(phi), p_ * sin(theta) * sin(phi),
+                p_ * cos(theta));
+        hPtr->Fill(sqrt(pow(p_ * sin(theta) * cos(phi), 2) +
+                        pow(p_ * sin(theta) * sin(phi), 2)));
         hE->Fill(pi.Energy());
         particle_v.push_back(pi);
       }
@@ -74,8 +76,10 @@ int main() {
         hPT->Fill(2);
         kaon->setCharge(charge);
         Particle ka{kaon, "kaon", ka_linearMomentum};
-        ka.setP(p_ * sin(theta) * cos(phi), p_ * sin(theta) * sin(phi), p_ * cos(theta));
-        hPtr->Fill(sqrt(pow(p_ * sin(theta) * cos(phi), 2) + pow(p_ * sin(theta) * sin(phi), 2)));
+        ka.setP(p_ * sin(theta) * cos(phi), p_ * sin(theta) * sin(phi),
+                p_ * cos(theta));
+        hPtr->Fill(sqrt(pow(p_ * sin(theta) * cos(phi), 2) +
+                        pow(p_ * sin(theta) * sin(phi), 2)));
         hE->Fill(ka.Energy());
         particle_v.push_back(ka);
       }
@@ -84,8 +88,10 @@ int main() {
         hPT->Fill(3);
         proton->setCharge(charge);
         Particle pr{proton, "proton", pr_linearMomentum};
-        pr.setP(p_ * sin(theta) * cos(phi), p_ * sin(theta) * sin(phi), p_ * cos(theta));
-        hPtr->Fill(sqrt(pow(p_ * sin(theta) * cos(phi), 2) + pow(p_ * sin(theta) * sin(phi), 2)));
+        pr.setP(p_ * sin(theta) * cos(phi), p_ * sin(theta) * sin(phi),
+                p_ * cos(theta));
+        hPtr->Fill(sqrt(pow(p_ * sin(theta) * cos(phi), 2) +
+                        pow(p_ * sin(theta) * sin(phi), 2)));
         hE->Fill(pr.Energy());
         particle_v.push_back(pr);
       }
@@ -93,8 +99,10 @@ int main() {
       else {
         hPT->Fill(4);
         Particle ks{K_s, "K*", ka_linearMomentum};
-        ks.setP(p_ * sin(theta) * cos(phi), p_ * sin(theta) * sin(phi), p_ * cos(theta));
-        hPtr->Fill(sqrt(pow(p_ * sin(theta) * cos(phi), 2) + pow(p_ * sin(theta) * sin(phi), 2)));
+        ks.setP(p_ * sin(theta) * cos(phi), p_ * sin(theta) * sin(phi),
+                p_ * cos(theta));
+        hPtr->Fill(sqrt(pow(p_ * sin(theta) * cos(phi), 2) +
+                        pow(p_ * sin(theta) * sin(phi), 2)));
         hE->Fill(ks.Energy());
         particle_v.push_back(ks);
       }
@@ -110,13 +118,13 @@ int main() {
     }
   }
 
-  TCanvas* cPT = new TCanvas("cPT", "Particle Types Distribution", 100, 100, 1100, 700);
+  TCanvas *cPT =
+      new TCanvas("cPT", "Particle Types Distribution", 100, 100, 1100, 700);
 
-  {
-    hPT->Draw();
-  }
+  { hPT->Draw(); }
 
-  TCanvas* cAngles = new TCanvas("cAngles", "Angles Distribution", 200, 100, 1100, 700);
+  TCanvas *cAngles =
+      new TCanvas("cAngles", "Angles Distribution", 200, 100, 1100, 700);
 
   {
     cAngles->Divide(1, 2);
@@ -130,7 +138,7 @@ int main() {
     hTheta->Draw();
   }
 
-  TCanvas* cPE = new TCanvas("cPE", "Momentum & Energy", 300, 100, 1100, 700);
+  TCanvas *cPE = new TCanvas("cPE", "Momentum & Energy", 300, 100, 1100, 700);
 
   {
     cPE->Divide(1, 3);
