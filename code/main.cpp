1a91de7b01d6d42e13e4ca82a8c0146866c364a2
samuelelanzi <samuelelanzi00@icloud.com>
2020-11-10 11:28:15 +0100

    upload

--- code/main.cpp
+++ code/main.cpp
@@ -1,21 +1,26 @@
+// header file
+#include "invMass.hpp"
 #include "Particle.hpp"
 #include "ParticleType.hpp"
 #include "ResonanceType.hpp"
-#include "invMass.hpp"
-#include "TRandom.h"
-#include "TCanvas.h"
-#include "TH1F.h"
-#include "TH1.h"
-#include "TStyle.h"
 #include "rndmCharge.hpp"
+
+// ROOT's header file
+#include "TCanvas.h"
+#include "TH1.h"
+#include "TH1F.h"
+#include "TRandom.h"
+#include "TStyle.h"
+
+// STL c++
 #include <random>
 
 int main() {
-  ParticleType* pion = new ParticleType {"pion", 0.13957, 0}; // 0 is default
-  ParticleType* kaon = new ParticleType {"kaon", 0.49367, 0};
-  ParticleType* proton = new ParticleType {"proton", 0.93827, 0};
-  ParticleType* K_s = new ParticleType {"K*", 0.89166, 0};
-  ResonanceType* K_resonance = new ResonanceType {*K_s, 0.050};
+  ParticleType* pion = new ParticleType{"pion", 0.13957, 0}; // 0 is default
+  ParticleType* kaon = new ParticleType{"kaon", 0.49367, 0};
+  ParticleType* proton = new ParticleType{"proton", 0.93827, 0};
+  ParticleType* K_s = new ParticleType{"K*", 0.89166, 0};
+  ResonanceType* K_resonance = new ResonanceType{*K_s, 0.050};
 
   std::vector<Particle> particle_v{};
 
@@ -27,9 +32,9 @@ int main() {
 
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_int_distribution<> distrib(1,100);
+  std::uniform_int_distribution<> distrib(1, 100);
   int seed = distrib(gen);
-  
+
   gRandom->SetSeed(seed);
 
   for (int i = 0; i != 1e5; ++i) {
@@ -43,71 +48,80 @@ int main() {
       hTheta->Fill(theta);
       double p_ = gRandom->Exp(1);
       hP->Fill(p_);
-      
+
       P pi_linearMomentum;
       P ka_linearMomentum;
       P pr_linearMomentum;
       P ks_linearMomentum;
 
-      if (prob_type <= 80) 
-      {
+      if (prob_type <= 80) {
         pion->setCharge(charge);
-        Particle pi {pion, "pion", pi_linearMomentum};
-        pi.setP(p_ * std::sin(theta) * std::cos(phi), p_ * std::sin(theta) * std::sin(phi), p_ * std::cos(theta));
-        hPtr->Fill(std::sqrt(std::pow(p_ * std::sin(theta) * std::cos(phi), 2) + std::pow(p_ * std::sin(theta) * std::sin(phi), 2)));
+        Particle pi{pion, "pion", pi_linearMomentum};
+        pi.setP(p_ * std::sin(theta) * std::cos(phi),
+                p_ * std::sin(theta) * std::sin(phi), p_ * std::cos(theta));
+        hPtr->Fill(
+            std::sqrt(std::pow(p_ * std::sin(theta) * std::cos(phi), 2) +
+                      std::pow(p_ * std::sin(theta) * std::sin(phi), 2)));
         hE->Fill(pi.Energy());
         particle_v.push_back(pi);
-      } 
+      }
 
-      else if (prob_type > 80 && prob_type <= 90) 
-      {
+      else if (prob_type > 80 && prob_type <= 90) {
         kaon->setCharge(charge);
-        Particle ka {kaon, "kaon", ka_linearMomentum};
-        ka.setP(p_ * std::sin(theta) * std::cos(phi), p_ * std::sin(theta) * std::sin(phi), p_ * std::cos(theta));
-        hPtr->Fill(std::sqrt(std::pow(p_ * std::sin(theta) * std::cos(phi), 2) + std::pow(p_ * std::sin(theta) * std::sin(phi), 2)));
+        Particle ka{kaon, "kaon", ka_linearMomentum};
+        ka.setP(p_ * std::sin(theta) * std::cos(phi),
+                p_ * std::sin(theta) * std::sin(phi), p_ * std::cos(theta));
+        hPtr->Fill(
+            std::sqrt(std::pow(p_ * std::sin(theta) * std::cos(phi), 2) +
+                      std::pow(p_ * std::sin(theta) * std::sin(phi), 2)));
         hE->Fill(ka.Energy());
         particle_v.push_back(ka);
-      } 
+      }
 
-      else if (prob_type > 90 && prob_type <= 99) 
-      {
+      else if (prob_type > 90 && prob_type <= 99) {
         proton->setCharge(charge);
-        Particle pr {proton, "proton", pr_linearMomentum};
-        pr.setP(p_ * std::sin(theta) * std::cos(phi), p_ * std::sin(theta) * std::sin(phi), p_ * std::cos(theta));
-        hPtr->Fill(std::sqrt(std::pow(p_ * std::sin(theta) * std::cos(phi), 2) + std::pow(p_ * std::sin(theta) * std::sin(phi), 2)));
+        Particle pr{proton, "proton", pr_linearMomentum};
+        pr.setP(p_ * std::sin(theta) * std::cos(phi),
+                p_ * std::sin(theta) * std::sin(phi), p_ * std::cos(theta));
+        hPtr->Fill(
+            std::sqrt(std::pow(p_ * std::sin(theta) * std::cos(phi), 2) +
+                      std::pow(p_ * std::sin(theta) * std::sin(phi), 2)));
         hE->Fill(pr.Energy());
         particle_v.push_back(pr);
-      } 
+      }
 
-      else 
-      {
-        Particle ks {K_s, "K*", ka_linearMomentum};
-        ks.setP(p_ * std::sin(theta) * std::cos(phi), p_ * std::sin(theta) * std::sin(phi), p_ * std::cos(theta));
-        hPtr->Fill(std::sqrt(std::pow(p_ * std::sin(theta) * std::cos(phi), 2) + std::pow(p_ * std::sin(theta) * std::sin(phi), 2)));
+      else {
+        Particle ks{K_s, "K*", ka_linearMomentum};
+        ks.setP(p_ * std::sin(theta) * std::cos(phi),
+                p_ * std::sin(theta) * std::sin(phi), p_ * std::cos(theta));
+        hPtr->Fill(
+            std::sqrt(std::pow(p_ * std::sin(theta) * std::cos(phi), 2) +
+                      std::pow(p_ * std::sin(theta) * std::sin(phi), 2)));
         hE->Fill(ks.Energy());
         particle_v.push_back(ks);
-      }      
+      }
     }
   }
 
   for (auto i : particle_v) {
-    if(i.getParticleType()->getName() == "K*") {
-      //i.Decay2body(pi_d, ka_d);
-      //particle_v.push_back(pi_d);
-      //particle_v.push_back(ka_d);
+    if (i.getParticleType()->getName() == "K*") {
+      // i.Decay2body(pi_d, ka_d);
+      // particle_v.push_back(pi_d);
+      // particle_v.push_back(ka_d);
       std::cout << "Found" << '\n';
     }
   }
 
-  TCanvas* cPT = new TCanvas("cPT", "Particle Types Distribution", 100, 100, 1100, 700 );
+  TCanvas *cPT =
+      new TCanvas("cPT", "Particle Types Distribution", 100, 100, 1100, 700);
 
-  TH1F* hPT = new TH1F("hPT", "Particle Types Distribution", 4, 0, 5);
+  TH1F *hPT = new TH1F("hPT", "Particle Types Distribution", 4, 0, 5);
   for (int i = 0; i != static_cast<int>(particle_v.size()); ++i) {
     if (particle_v[i].getParticleType()->getName() == "pion") {
       hPT->Fill(1);
     } else if (particle_v[i].getParticleType()->getName() == "kaon") {
       hPT->Fill(2);
-    } else if(particle_v[i].getParticleType()->getName() == "proton") {
+    } else if (particle_v[i].getParticleType()->getName() == "proton") {
       hPT->Fill(3);
     } else {
       hPT->Fill(4);
@@ -115,7 +129,8 @@ int main() {
   }
   hPT->Draw();
 
-  TCanvas* cAngles = new TCanvas ("cAngles", "Angles Distribution", 200, 100, 1100, 700);
+  TCanvas *cAngles =
+      new TCanvas("cAngles", "Angles Distribution", 200, 100, 1100, 700);
   cAngles->Divide(1, 2);
 
   cAngles->cd(1);
@@ -126,10 +141,10 @@ int main() {
   hTheta->Fit("pol0");
   hTheta->Draw();
 
-  TCanvas* cPE = new TCanvas("cPE", "Momentum & Energy", 300, 100, 1100, 700);
+  TCanvas *cPE = new TCanvas("cPE", "Momentum & Energy", 300, 100, 1100, 700);
   cPE->Divide(1, 3);
 
-  gStyle->SetOptStat(112210); 
+  gStyle->SetOptStat(112210);
   gStyle->SetOptFit(111);
 
   cPE->cd(1);
@@ -140,5 +155,5 @@ int main() {
   hPtr->Draw();
 
   cPE->cd(3);
-  hE->Draw(); 
+  hE->Draw();
 }
