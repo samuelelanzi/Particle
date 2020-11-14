ea5cb7021ce09191982729bc28cb8985622e5487
samuelelanzi <samuelelanzi00@icloud.com>
2020-11-14 20:40:50 +0100

    update

--- code/runROOT.sh
+++ code/runROOT.sh
@@ -3,7 +3,6 @@ gROOT->LoadMacro("ParticleType.cpp+")
 gROOT->LoadMacro("ResonanceType.cpp+")
 gROOT->LoadMacro("Particle.cpp+")
 gROOT->LoadMacro("main.cpp+")
-gROOT->LoadMacro("rndmCharge.cpp+")
 gROOT->LoadMacro("invMass.cpp+")
 EOF
 root -l
