dbd305101234a9cfe5bc115db42fb2d3829bae58
samuelelanzi <samuelelanzi00@icloud.com>
2021-01-07 22:23:50 +0100

    upload

--- code/runROOT.sh
+++ code/runROOT.sh
@@ -3,6 +3,5 @@ gROOT->LoadMacro("ParticleType.cpp+")
 gROOT->LoadMacro("ResonanceType.cpp+")
 gROOT->LoadMacro("Particle.cpp+")
 gROOT->LoadMacro("main.cpp+")
-gROOT->LoadMacro("invMass.cpp+")
 EOF
 root -l
